8915ffc41cf5dff0985c189e56cf4e9cdac240b5
Marvine Baratto <marvin.baratto@gmail.com>
2021-11-18 17:26:54 +0100

    18/11/2021

---
+++ 2- Composants d'interface/2 - Interface Desktop/2- WPF/Gestion de produits/Actions.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Gestion_de_produits
+{
+    /// <summary>
+    /// Logique d'interaction pour Actions.xaml
+    /// </summary>
+    public partial class Actions : Window
+    {
+        MainWindow Window;
+        Produits Produit;
+        public Actions(object sender, MainWindow window, Produits p)
+        {
+            InitializeComponent();
+            this.Produit = p;
+            this.Window = window;
+            InitPage(sender);
+        }
+
+        private void InitPage(object sender)
+        {
+            string nom = (string) ((Button)sender).Content;
+            labTitreAction.Content = nom + " un produit";
+            
+            switch (nom)
+            {
+                case "Ajouter":
+                    //Button valider
+                    btnValider.Click += (s,e) =>AjouterProduit();
+                    break;
+                case "Modifier":
+                    //Id Produit
+                    labIdProduit.Visibility = Visibility.Hidden;
+                    txbIdProduit.Visibility = Visibility.Hidden;
+                    //Libelle
+                    txbLibelle.Text = Produit.Libelle;
+                    //Rayon
+                    txbRayon.Text = Produit.Rayon.ToString();
+                    //Categorie
+                    txbCategorie.Text = Produit.Categorie;
+                    //Button valider
+                    btnValider.Click += (s, e) => ModifierProduit();
+                    break;
+                case "Supprimer":
+                    //Id Produit
+                    txbIdProduit.Text = Produit.IdProduit.ToString();
+                    txbIdProduit.IsEnabled = false;
+                    //Libelle
+                    txbLibelle.Text = Produit.Libelle;
+                    txbLibelle.IsEnabled = false;
+                    //Rayon
+                    txbRayon.Text = Produit.Rayon.ToString();
+                    txbRayon.IsEnabled = false;
+                    //Categorie
+                    txbCategorie.Text = Produit.Categorie;
+                    txbCategorie.IsEnabled = false;
+                    //Button valider
+                    btnValider.Click += (s, e) => SupprimerProduit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        private void AjouterProduit()
+        {
+            Produit = new Produits(int.Parse(txbIdProduit.Text),txbLibelle.Text,txbCategorie.Text,int.Parse(txbRayon.Text));
+            //Ajout du nouveau produit
+            this.Window.AjouterProduit(Produit);
+
+            //Retour à la fenetre principale
+            Retour();
+        }
+
+        private void SupprimerProduit()
+        {
+            //Ajout du nouveau produit
+            this.Window.SupprimerProduit(Produit);
+
+            //Retour à la fenetre principale
+            Retour();
+        }
+
+        private void ModifierProduit()
+        {
+            Produit.Libelle = txbLibelle.Text;
+            Produit.Rayon = int.Parse(txbRayon.Text);
+            Produit.Categorie = txbCategorie.Text;
+            //Ajout du nouveau produit
+            this.Window.ModifierProduit(Produit);
+
+            //Retour à la fenetre principale
+            Retour();
+        }
+
+        public void Retour(object sender, RoutedEventArgs e)
+        {    
+           this.Close();
+        }
+
+        public void Retour()
+        { 
+            this.Close();
+        }
+
+    }
+}
--- 2- Composants d'interface/2 - Interface Desktop/2- WPF/Gestion de produits/MainWindow.xaml.cs
+++ 2- Composants d'interface/2 - Interface Desktop/2- WPF/Gestion de produits/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,108 @@ namespace Gestion_de_produits
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Produits> liste;
+        string path = @"../../Produits.json";
+
         public MainWindow()
         {
             InitializeComponent();
+            //liste = CreerListe(); // Pour initialiser le jeu de données
+            liste = TransformeJson();
+            RemplirGrid();
+            CreerFichier();
         }
+        public void RemplirGrid()
+        {
+            dataProduit.ItemsSource = liste;
+        }
+
+        private List<Produits> CreerListe()
+        {
+            List<Produits> liste = new List<Produits>();
+
+            for (int i = 1; i < 15; i++)
+            {
+                Produits p = new Produits(1, "Produit" + i, "Catégorie "+i,i+1);
+                liste.Add(p);
+            }
+            return liste;
+        }
+
+        private void CreerFichier()
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(liste, Formatting.Indented));
+        }
+
+        private string LireFichier()
+        // Renvoi un tableau de chaine contenant les informations stockées dans le fichier 
+        {
+            string chaine;
+            try
+            {
+                // Lecture et stockage dans chaine
+                chaine = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Une exception s'est produite : " + e.Message);
+                Console.WriteLine("Indiquer le path :");
+                path = Console.ReadLine();
+                chaine = LireFichier();
+            }
+            return chaine;
+        }
+
+        private List<Produits> TransformeJson()
+        {
+            string chaine = LireFichier();
+            List<Produits> liste = JsonConvert.DeserializeObject<List<Produits>>(chaine);
+            return liste;
+        }
+
+        private void btnActions_Click(object sender, RoutedEventArgs e)
+        {
+            Produits p = (Produits)dataProduit.SelectedItem;
+            string nom = (string)((Button)sender).Content;
+            if (p == null && ( nom=="Modifier" || nom== "Supprimer") )
+            {
+                MessageBox.Show("Pas de sélection");
+            }
+            else
+            {
+                Actions actions = new Actions(sender, this, p);
+                this.Opacity = 0.7;
+                actions.ShowDialog();
+                this.Opacity = 1;
+            }
+            
+        }
+
+        public void AjouterProduit(Produits p)
+        {
+            liste.Add(p);
+            ActualiserTableau();
+        }
+
+        public void ModifierProduit(Produits p)
+        {
+            int index=liste.FindIndex(x => x.IdProduit == p.IdProduit);
+            this.liste[index]=p;
+            ActualiserTableau();
+        }
+
+        public void SupprimerProduit(Produits p)
+        {
+            int index = liste.FindIndex(x => x.IdProduit == p.IdProduit);
+            this.liste.RemoveAt(index);
+            ActualiserTableau();
+        }
+
+        private void ActualiserTableau()
+        {
+            dataProduit.Items.Refresh();
+            CreerFichier();
+        }
+
     }
 }
