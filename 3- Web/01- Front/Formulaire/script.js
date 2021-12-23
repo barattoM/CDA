e4197f2fc1c2eee52c3a59e295a13c96371575ab
Marvine Baratto <marvin.baratto@gmail.com>
2021-12-23 17:27:18 +0100

    23/12/2021

---
+++ 3- Web/01- Front/Formulaire/script.js
@@ -0,0 +1,117 @@
+var infos=document.querySelectorAll(".iconeInfo");
+var mdp=document.getElementById("mdp");
+var infoMDP=document.getElementById("infoMDP");
+var listeInputs=document.querySelectorAll("input[type=text]");
+var valide = {} // contient false si l'input ne respecte pas la regex, true sinon
+var boutonValider=document.querySelector("input[type=submit]")
+
+//Initialisation des regex du MDP
+var RegexMDP=mdp.pattern.split(')'); // On récupère toute les conditions du regex du mdp
+for(i=0;i<RegexMDP.length-1;i++){ //On ajoute la fermeture de parenthèse enlever avec le split au élément qui en ont besoin (pas la dernière qui est le nombre de caractères) et on les transforme en Regex
+    RegexMDP[i]=RegExp(RegexMDP[i]+")");
+}
+RegexMDP[RegexMDP.length-1]=RegExp(RegexMDP[RegexMDP.length-1]);//On transforme la dernière condition en Regex
+
+//Validation des Inputs
+listeInputs.forEach(element => {
+    element.addEventListener("input",check);
+    // on ajoute un attribut à l'objet valide pour chaque input
+    if (element.required)
+        valide[element.name] = false;
+    else {
+        valide[element.name] = true;
+    }
+});
+
+
+//Affichage des informations au survol des icones ?
+infos.forEach(element => {
+    element.addEventListener("mouseover",function(e){
+        e.target.parentNode.nextElementSibling.classList.remove("cacher");
+    })
+    element.addEventListener("mouseout",function(e){
+        e.target.parentNode.nextElementSibling.classList.add("cacher");
+    })
+});
+
+//Affichage des informations sur le champs MDP
+mdp.addEventListener("focus",function(e){
+    infoMDP.classList.remove("cacher");
+});
+mdp.addEventListener("focusout",function(e){
+    infoMDP.classList.add("cacher");
+});
+//Event qui check les different regex du MDP et le valide si ils sont bon
+mdp.addEventListener('input',checkMDP);
+valide["password"]=false; //ajout du password dans la liste des validations
+
+/**
+ * Check la validé de l'input (text) et affiche l'icone d'erreur ou validation
+ * @param {*} e 
+ */
+function check(e){
+    input = e.target;
+    icone=e.target.nextElementSibling.querySelector(".iconeErreur");
+    if (input.checkValidity()) {
+        icone.setAttribute("class","fas fa-check-circle green iconeErreur")
+        valide[input.name] = true;
+    } else {
+        icone.setAttribute("class","fas fa-times-circle red iconeErreur")
+        valide[input.name] = false;
+    }
+    checkForm();
+}
+
+/**
+ * Check si le formulaire est bien remplis et active ou desactive le bouton en conséquence
+ */
+function checkForm(){
+    boutonValider.disabled = false;
+    boutonValider.nextElementSibling.children[0].classList.remove("cacher")
+    // Object.values(valide) : transforme l'objet en tableau
+    //.indexOf(false) : cherche la position de false dans le tableau
+    // si Object.values(valide).indexOf(false) est different de -1, ca veut dire qu'il a trouvé un false dans le tableau
+    // donc l'un des input ne respecte pas la regex
+    if (Object.values(valide).indexOf(false) != -1) {
+        boutonValider.disabled = true;
+        boutonValider.nextElementSibling.children[0].classList.add("cacher")
+    }
+}
+
+/**
+ * check les different regex du mdp, et change sa valeur dans le tableau valideMDP et affiche l'icone en conséquence 
+ */
+function checkMDP(e){
+
+    // Si un caractère est un chiffre
+   if(RegexMDP[0].test(mdp.value)){ 
+        infoMDP.children[1].children[2].children[0].setAttribute("class","fas fa-check-circle green");
+   }else{
+        infoMDP.children[1].children[2].children[0].setAttribute("class","fas fa-times-circle red");
+   }
+   // Si un caractère est une majuscule
+    if(RegexMDP[1].test(mdp.value)){ 
+        infoMDP.children[1].children[0].children[0].setAttribute("class","fas fa-check-circle green");
+    }else{
+        infoMDP.children[1].children[0].children[0].setAttribute("class","fas fa-times-circle red");
+    }
+    // Si un caractère est une minuscule
+    if(RegexMDP[2].test(mdp.value)){ 
+        infoMDP.children[1].children[1].children[0].setAttribute("class","fas fa-check-circle green");
+    }else{
+        infoMDP.children[1].children[1].children[0].setAttribute("class","fas fa-times-circle red");
+    }
+    // Si un caractère est un caractère spécial
+    if(RegexMDP[3].test(mdp.value)){ 
+        infoMDP.children[1].children[3].children[0].setAttribute("class","fas fa-check-circle green");
+    }else{
+        infoMDP.children[1].children[3].children[0].setAttribute("class","fas fa-times-circle red");
+    }
+    // Si il y a 8 caractère
+    if(RegexMDP[4].test(mdp.value)){ 
+        infoMDP.children[1].children[4].children[0].setAttribute("class","fas fa-check-circle green");
+    }else{
+        infoMDP.children[1].children[4].children[0].setAttribute("class","fas fa-times-circle red");
+    }
+    check(e);
+}
