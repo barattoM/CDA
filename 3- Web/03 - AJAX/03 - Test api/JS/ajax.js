d03c13c597bcf6ec588e4527359199c6b3f43225
Marvine Baratto <marvin.baratto@gmail.com>
2022-01-24 17:33:00 +0100

    24/01/2022

---
+++ 3- Web/03 - AJAX/03 - Test api/JS/ajax.js
@@ -0,0 +1,56 @@
+// Utilisation de l'Ajax pour appeler l'API et récuperer les enregistrements
+var contenu = document.getElementById("divContenu");
+var boutton=document.getElementById("ok");
+var inputs= document.querySelectorAll("input");
+
+// on définit une requete
+const req = new XMLHttpRequest();
+
+//on envoi la requête
+
+req.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=wNRj67i7e4U5BfKIloj24kFEGMdZfict1kgs8XRd', true);
+req.send(null);
+
+
+//on vérifie les changements d'états de la requête
+req.onreadystatechange = function (event) {
+    if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+            // la requete a abouti et a fournit une reponse
+            //on décode la réponse, pour obtenir un objet
+            reponse = JSON.parse(this.responseText);
+            console.log(this.responseText);
+            console.log(reponse);
+            reponses= [];
+            contenu.innerHTML="";
+            if(!Array.isArray(reponse)){
+                reponses[0]=reponse;
+            }else{
+                reponses=reponse;
+            }
+            console.log(reponses);
+            for (let i = 0; i < reponses.length; i++) {
+                bloc=document.createElement("div");
+                bloc.className= "colonne";
+                date=document.createElement("div");
+                date.className= "center";
+                date.innerHTML=reponses[i].date;
+                img=document.createElement("img");
+                img.setAttribute("src",reponses[i].url);
+                bloc.appendChild(date);
+                bloc.appendChild(img);
+                contenu.appendChild(bloc);
+            }
+            console.log("Réponse reçue: %s", this.responseText);
+        } else {
+            console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+        }
+    }
+};
+
+boutton.addEventListener("click",function(e) {
+    url="https://api.nasa.gov/planetary/apod?api_key=wNRj67i7e4U5BfKIloj24kFEGMdZfict1kgs8XRd&start_date="+inputs[0].value+"&end_date="+inputs[1].value;
+    req.open('GET', url, true);
+    req.send(null);
+});
+
---
+++ 3- Web/03 - AJAX/03 - Test api/PHP/View/Principale.php
@@ -0,0 +1,14 @@
+﻿
+<!-- Cette page est le contenu de la page principale du site; Elle présente la liste des parkings. -->
+
+<div id="divSousTitre">
+	<h3>Liste</h3>
+</div>
+<label for="debut">Date de début</label>
+<input type="date">
+<label for="fin">Date de fin</label>
+<input type="date">
+<button id="ok">Valider</button>
+<div id="divContenu">
+	
+</div>
